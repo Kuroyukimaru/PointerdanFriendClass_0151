aebfb1d8fb88d7ee46688fc9f32ed6215e68da77
Kuroyukimaru <oranggabut712@gmail.com>
2024-05-07 10:46:16 +0700

    mengubah nilai nim menjadi 2

--- PointerdanFriendclass_0151/PointerdanFriendclass_0151.cpp
+++ PointerdanFriendclass_0151/PointerdanFriendclass_0151.cpp
@@ -13,4 +13,8 @@ int main() {
     mahasiswa mhs{ 1 };     // Objek mhs
     mhs.showNim();      //Member Acces Operator
 
+    mahasiswa& refMhs = mhs;        //Pointer Reference refMhs
+    refMhs.nim = 2;     //Member Access Operator
+    mhs.showNim();
+
 }
